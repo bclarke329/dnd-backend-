9f64d82329e3ee6cd5b194bb9225b0e713fe1252
bclarke329 <bmendoza329@hotmail.com>
2021-05-01 18:08:23 -0500

    got rid of instance variables

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,47 +6,48 @@ class CharactersController < ApplicationController
   before_action :set_character, only: [:show, :update, :destroy]
   # GET /characters
   def index
-    @characters = Character.all
+    characters = Character.all
 
-    render json: @characters
+    render json: characters
   end
 
   # GET /characters/1
   def show
+    character = character.find(params[:id])
     render json: @character
   end
 
   # POST /characters
   def create
-    @party = Party.find_by_id(params[:id])
-    @character = Character.new(character_params)
-    @character.party = @party
-
-    if @character.save
-      render json: @character, status: :created, location: @character
+    party = Party.find_by_id(params[:id])
+    character = Character.new(character_params)
+    character.party = party
+    character.save
+    if character.save
+      render json: character, status: :created, location: character
     else
-      render json: @character.errors, status: :unprocessable_entity
+      render json: character.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /characters/1
   def update
-    if @character.update(character_params)
-      render json: @character
+    if character.update(character_params)
+      render json: character
     else
-      render json: @character.errors, status: :unprocessable_entity
+      render json: character.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /characters/1
   def destroy
-    @character.destroy
+    character.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_character
-      @character = Character.find(params[:id])
+      character = Character.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,4 @@
 class Character < ApplicationRecord
-    belongs_to :party
+    belongs_to :party, optional: true
     validates :name, uniqueness: true
 end
